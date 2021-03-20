4b93e0bf2bff2b20b704a0af49fbe0fdb4e5ef2b
DGR22419 <75600994+dev22419@users.noreply.github.com>
2021-03-20 13:50:58 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,4 +6,4 @@ mv tts /data/data/com.termux/files/usr/bin/
 
 mv speak /data/data/com.termux/files/usr/share/
 
-rm -r -f /data/data/com.termux/files/home/tts
+rm -r -f /data/data/com.termux/files/home/termux-tts
